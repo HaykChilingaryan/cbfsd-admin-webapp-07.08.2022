08cfb6c6e0a04908b6da29d869b5b9a533017c68
Hayk Chilingaryan <haykchilingaryan2002@gmail.com>
2022-08-13 09:38:18 -0700

    Added Homepage design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { ContactusComponent } from './components/contactus/contactus.component';
+
 import { FooterComponent } from './components/footer/footer.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
@@ -15,17 +18,21 @@ import { UsersModule } from './components/users/users.module';
 import { ProductsModule } from './components/products/products.module';
 import { OrdersModule } from './components/orders/orders.module';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
+    AboutusComponent,
+    ContactusComponent,
     HeaderComponent,
     HomeComponent,
     FooterComponent,
     NavbarComponent,
     SideNavbarComponent,
     ProductCategoriesComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    
   ],
   imports: [
     BrowserModule,
